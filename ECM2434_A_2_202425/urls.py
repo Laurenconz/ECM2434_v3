4cfc7968673560389af5c5103d379f6f7aac1272
codermatcha <stl214@exeter.ac.uk>
2025-03-25 22:20:17 +0000

    Integrate React frontend with proper file structure

--- ECM2434_A_2_202425/urls.py
+++ ECM2434_A_2_202425/urls.py
@@ -3,19 +3,16 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-from django.http import HttpResponse
-
-def test_view(request):
-    return HttpResponse("<html><body><h1>Django is working!</h1><p>This test view confirms the basic functionality.</p></body></html>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('bingo.urls')),
-    path('test/', test_view),
+    
+    # Serve the React app
     path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!api/|admin/).*$', TemplateView.as_view(template_name='index.html')),
 ]
 
-# Add this section to serve static files during development
+# Add this for development (will be ignored in production with DEBUG=False)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
