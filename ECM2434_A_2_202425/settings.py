27a5a6d63ef69d17fe6e43d5242ab2a13cd7228d
codermatcha <stl214@exeter.ac.uk>
2025-03-25 21:33:50 +0000

    test 2

--- ECM2434_A_2_202425/settings.py
+++ ECM2434_A_2_202425/settings.py
@@ -124,8 +124,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'bingo-frontend', 'build'),
-    os.path.join(BASE_DIR, 'bingo-frontend', 'build', 'static'),
 ]
 
 # Media files
