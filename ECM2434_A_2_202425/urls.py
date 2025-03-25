e95f5a71ffd372eca4d4b227e5d35fe5a4df1b1e
codermatcha <stl214@exeter.ac.uk>
2025-03-25 22:04:30 +0000

    Add test view and React build integration

--- ECM2434_A_2_202425/urls.py
+++ ECM2434_A_2_202425/urls.py
@@ -7,10 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('bingo.urls')),
-    
-    # Serve React app - this should be the last pattern
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
+
 # Add this section to serve static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
